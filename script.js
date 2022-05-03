6eecb7d83fd4f709e5f53abbf221ba904dc351e3
Kate Danilova <ecdanilova@gmail.com>
2022-05-03 00:13:56 -0400

    add script

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ function showTip(e) {
     tip.style.display = "block";
     e.preventDefault();
 }
+
+
+function calculateAmount (e) {
+    e.preventDefault(); 
+    const bill = document.querySelector("#bill").value;
+    const people = document.querySelector("#people").value;
+    const tip = document.querySelector("#tip").value;
+
+    if (bill === "" || people === "" || people < 1) { 
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Please enter your information!',
+          })
+    }
+
+    let amountPerPerson = bill/people;
+    let tipPerPerson = (bill * tip) / people;
+    let totalSum = amountPerPerson + tipPerPerson;
+
+    amountPerPerson = amountPerPerson.toFixed(2);
+    tipPerPerson = tipPerPerson.toFixed(2);
+    totalSum = totalSum.toFixed(2);
+
+    document.querySelector("#dividedBill").textContent = amountPerPerson;
+    document.querySelector("#dividedTip").textContent = tipPerPerson;
+    document.querySelector("#billAndTip").textContent = totalSum;
+}
